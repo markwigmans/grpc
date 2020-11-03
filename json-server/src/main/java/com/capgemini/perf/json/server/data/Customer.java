66b683abd66d4a9ddf3aa0dc64311e690e8b90a9
Mark Wigmans <mark.wigmans@capgemini.com>
2020-11-03 09:20:47 +0100

    Add uniqueness constraint

--- json-server/src/main/java/com/capgemini/perf/json/server/data/Customer.java
+++ json-server/src/main/java/com/capgemini/perf/json/server/data/Customer.java
@@ -4,6 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
@@ -18,6 +19,7 @@ public class Customer {
     @Id
     @GeneratedValue
     private UUID id;
+    @Column(unique = true, nullable = false)
     private int userId;
     private String ref;
     private String name;
